ed9d3ef56ed63e590b8803758f650cb3f86cfbc2
sherifbai <godness980@gmail.com>
2022-01-16 21:27:00 +0300

    added component header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+    function Header({ text, bgColor, textColor }) {
+    const headerStyle = {
+        backgroundColor: bgColor,
+        color: textColor
+    }
+
+    return (
+        <header style={headerStyle}>
+            <div className="container">
+                <h2>{ text }</h2>
+            </div>
+        </header>
+    );
+}
+
+Header.defaultProps = {
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95'
+}
+
+export default Header;
